24b38bcb70c8d8419cf08844a3c9731a8bc1ac09
wwjk4628 <163090464+wwjk4628@users.noreply.github.com>
2024-06-27 16:46:53 +0900

    asd

--- src/main/java/himedia/myportal/controllers/MainController.java
+++ src/main/java/himedia/myportal/controllers/MainController.java
@@ -1,5 +1,7 @@
 package himedia.myportal.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,27 +13,29 @@ import himedia.myportal.exceptions.MainControllerException;
 
 @Controller
 public class MainController {
-	@RequestMapping({"/", "/main"})
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+	@RequestMapping({ "/", "/main" })
 	public String main() {
-		
+		logger.debug("마이포털 메인 페이지");
 //		return "/WEB-INF/views/home.jsp";
 		return "home";
 	}
-	
+
 	@GetMapping("/except")
 	@ResponseBody
 	public String raiseExcept() {
-		//	RuntimeException -> 좀 더 구체적인 예외로 전환한 이후 throw 해준다
+		// RuntimeException -> 좀 더 구체적인 예외로 전환한 이후 throw 해준다
 //		throw new RuntimeException("force Exception");
 		throw new MainControllerException("메인 컨트롤러에서 예외가 발생했습니다!");
 	}
-	
+
 //	@ExceptionHandler(RuntimeException.class)
 //	@ResponseBody
 //	public String handlerControllerException(RuntimeException e) {
 //		return "Exception: " + e.getMessage();
 //	}
-	
+
 //	@ExceptionHandler(MainControllerException.class)
 //	public String handleControllerExcept(MainControllerException e,
 //			Model model) {
